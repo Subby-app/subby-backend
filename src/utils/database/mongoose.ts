2f2b5768b4b6b7bc6593c19e0c60da9f157b7901
Umoh Samuel <samuelwisdom360@gmail.com>
2024-01-04 11:03:30 +0100

    setup mongodb connection

---
+++ src/utils/database/mongoose.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+export const mongooseConnect = () => {
+  const { MONGODB_URI } = process.env;
+  mongoose
+    .connect(`${MONGODB_URI}`)
+    .then(() => console.log("connected to mongodb")) //!use logger
+    .catch((err) => {
+      console.log("failed to connect to mongodb")
+      handleReconnection()
+    });
+}
+
+const handleReconnection = () => {
+  setTimeout(() => {
+    console.log("reconnecting to mongodb...")
+    mongooseConnect();
+  }, 10000); //!use env var
+}
