af8505521f155842733063ed34acfba61bf2b29e
Umoh Samuel <samuelwisdom360@gmail.com>
2024-01-07 15:47:16 +0100

    implement base AuthService

---
+++ src/resources/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { HttpException, HttpStatus } from '@/utils/exceptions';
+import { UserService } from '../user/user.service';
+import * as token from '@/utils/token.util';
+import { generateOtp } from '@/utils/otp.util';
+
+class AuthService {
+  private UserService = new UserService();
+
+  public async register(
+    email:string,
+    firstName: string,
+    lastName: string,
+    password: string,
+    username: string,
+    phoneNumber: string,
+  ) {
+    return await this.UserService.register(email, firstName, lastName, password, username, phoneNumber);
+  }
+
+  public async login(email: string, password: string) {
+    const user = await this.UserService.getFullUser({email});
+    if (!(await user.isValidPassword(password))) {
+      throw new HttpException(HttpStatus.BAD_REQUEST, 'invalid email or password');
+    }
+    return {
+      accessToken: token.createToken({id: user._id}),
+    };
+  }
+
+  public async generateOtp(email: string) {
+    const user = await this.UserService.getFullUser({email});
+
+    user.otp = generateOtp();
+    // !handle otp expiration
+    user.save();
+    return { otpGenerated: true, email: user.email};
+  }
+
+  public async validateOtp(email: string, otp: string) {
+    const user = await this.UserService.getFullUser({email});
+
+    if (otp !== user.otp) {
+      // !handle otp expiration
+      throw new HttpException(HttpStatus.BAD_REQUEST, 'otp expired or invalid');
+    }
+    return {validOtp: true};
+  }
+}
+
+export { AuthService };
