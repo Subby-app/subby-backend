4c704245ff33b7075b760d5e1a47c9d6f6322a2c
Umoh Samuel <samuelwisdom360@gmail.com>
2024-01-08 13:06:38 +0100

    fix: move auth code from admin controller to auth controller

---
+++ src/resources/auth/auth.controller.ts
@@ -0,0 +1,114 @@
+import { Request, Response, NextFunction } from 'express';
+import { AuthService } from './auth.service';
+import { HttpStatus } from '@/utils/exceptions';
+
+const authService = new AuthService();
+
+export async function signup(req: Request, res: Response, next: NextFunction) {
+  try {
+    const { email, firstName, lastName, password, username, phoneNumber } = req.body;
+    const data = await authService.register(email, firstName, lastName, password, username, phoneNumber);
+
+    res.status(HttpStatus.OK).json({
+      message: 'successful signup',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function login(req: Request, res: Response, next: NextFunction) {
+  try {
+    const {email, password} = req.body;
+    const data = await authService.login(email, password);
+
+    res.status(HttpStatus.OK).json({
+      message: 'successful login',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function sendOtp(req: Request, res: Response, next: NextFunction) {
+  try {
+    const data = await authService.sendOtp(req.body.email);
+
+    res.status(HttpStatus.OK).json({
+      message: 'otp has been sent',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function verifyOtp(req: Request, res: Response, next: NextFunction) {
+  try {
+    const {email, otp} = req.body;
+    const data = await authService.verifyOtp(email, otp);
+
+    res.status(HttpStatus.OK).json({
+      message: 'otp is valid',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function verifyAccount(req: Request, res: Response, next: NextFunction) {
+  try {
+    const data = await authService.verifyAccount(req.body.email);
+
+    res.status(HttpStatus.OK).json({
+      message: 'an otp has been to your email',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function verifyEmail(req: Request, res: Response, next: NextFunction) {
+  try {
+    const {email, otp} = req.body;
+    const data = await authService.verifyEmail(email, otp);
+
+    res.status(HttpStatus.OK).json({
+      message: 'your account is verified',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function resetPasswordRequest(req: Request, res: Response, next: NextFunction) {
+  try {
+    const data = await authService.resetPasswordRequest(req.body.email);
+
+    res.status(HttpStatus.OK).json({
+      message: 'an otp has been sent to your email',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function resetPassword(req: Request, res: Response, next: NextFunction) {
+  try {
+    const {email, newPassword} = req.body;
+    const data = await authService.resetPassword(email, newPassword);
+
+    res.status(HttpStatus.OK).json({
+      message: 'password reset successful',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
