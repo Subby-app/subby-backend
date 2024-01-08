d40a454033523cd564afc3b9c008f88a201e029f
Umoh Samuel <samuelwisdom360@gmail.com>
2024-01-08 12:48:59 +0100

    setup auth middleware using bearerAuth format

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from 'express';
+import { verifyToken, Token } from '@/utils/token.util';
+import { UserModel } from '@/resources/user/user.model';
+import { HttpException, HttpStatus } from '@/utils/exceptions/index';
+import jwt from 'jsonwebtoken';
+
+/**
+ * Checks if the incoming request has authorization bearer header.
+ */
+export async function authenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<Response | void> {
+  const authError = new HttpException(HttpStatus.UNAUTHORIZED, 'you are not authorized');
+  try {
+    const bearer = req.headers.authorization;
+
+    if (!bearer || !bearer.startsWith('Bearer ')) {
+      return next(authError);
+    }
+
+    const accessToken = bearer.split('Bearer ')[1].trim();
+
+    const payload: Token | jwt.JsonWebTokenError = await verifyToken(accessToken);
+
+    if (payload instanceof jwt.JsonWebTokenError) {
+      return next(authError);
+    }
+
+    const user = await UserModel.findById(payload.id).exec();
+
+    if (!user) {
+      return next(new HttpException(HttpStatus.NOT_FOUND, 'user not found'));
+    }
+
+    req.user = user;
+    return next();
+  } catch (error) {
+    return next(authError);
+  }
+}
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,9 @@
 import { errorMiddleware } from './error.middleware';
 import { handleInvalidRoutes } from './invalid-route.middleware';
+import { authenticated } from './auth.middleware';
 
 export {
   errorMiddleware,
   handleInvalidRoutes,
+  authenticated,
 }
