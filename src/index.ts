c3f4e6fe757eeb356dc147562a806eeaa758d9ed
Prince Uche Ikechukwu <54849212+Purch1@users.noreply.github.com>
2024-01-28 00:25:39 +0100

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,6 @@ import { mongooseConnect } from './data/database/mongoose';
 import { App } from './app';
 import { apiRouters } from './web/routes/index';
 
-process.on('SIGINT', () => {
-  console.log('Received SIGINT. Closing server...');
-  // Perform cleanup tasks if necessary
-  process.exit(0);
-});
-
-process.on('SIGTERM', () => {
-  console.log('Received SIGTERM. Closing server...');
-  // Perform cleanup tasks if necessary
-  process.exit(0);
-});
-
 mongooseConnect();
 
 const app = new App(apiRouters, +process.env.PORT!);
