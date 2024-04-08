9dab25463ed5ef507ba6ae190b9c9a89e5aee701
purch1 <ucheprince26@gmail.com>
2024-04-09 00:34:42 +0100

    Modified cors config

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,13 +1,11 @@
 import 'dotenv/config';
 import { CorsOptions } from 'cors';
 
-const whitelist = [
-  'https://subby-frontend-alpha.vercel.app',
-  'http://localhost:3000',
-  'http://localhost:5173',
-];
-
 export const corsOptions: CorsOptions = {
-  origin: whitelist,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  origin: [
+    'https://subby-frontend-alpha.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:5173',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
 };
